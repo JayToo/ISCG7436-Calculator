def25b2f6246a0b9236f3f2f48b3527467b3154f
Jayson <jayson.jury@gmail.com>
2019-09-23 21:41:17 +1200

    padding increase of the super stack view and linking the calculator label to an IBOutlet

--- ISCG7436 Calculator/ViewController.swift
+++ ISCG7436 Calculator/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlets
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultLabel.text = " it is working"
     }
-
+    //IBActions
 
 }
 
